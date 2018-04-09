2d22ac08854939540b611a6090665c5aa5c2c7f8
ke_ix1 <xkei4dev@gmail.com>
2018-04-09 23:59:40 +0900

    add google search function

--- web/expand_search/views.py
+++ web/expand_search/views.py
@@ -2,14 +2,33 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import sys,os
 sys.path.append('expand_search/modules/testmodu')
+sys.path.append('expand_search/modules/search')
 from modu import hello
+from expandsearch import ExpandSearch
+from .forms import MyForm
 
 def index(request):
+
+    app = hello()
+    view_list = []
     # return HttpResponse("Hello, world. You're at the blog index.")
+    if request.method == "POST":
+        form = MyForm(data=request.POST)  # ← 受け取ったPOSTデータを渡す
+        if form.is_valid():  # ← 受け取ったデータの正当性確認
+            text = request.POST['text']  # ← 正しいデータを受け取った場合の処理
+            es = ExpandSearch()
+            es_list = es.get_expand(text)
+            view_list = es_list
+
+    else:  # ← methodが'POST'ではない = 最初のページ表示時の処理
+        form = MyForm()
+
     myapp_data = {
-    'app': hello(),
-    'num': range(10),
+    'app': app,
+    'view_list': view_list,
+    'form': form
     }
+ 
     return render(request, 'index.html', myapp_data)
 
 def getExpand(request):
